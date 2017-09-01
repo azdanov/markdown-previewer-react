9263894360fb9e302ad72a7452a031b5a265d328
Anton Zdanov <akumael@gmail.com>
2017-09-01 15:31:38 +0300

    Add flow support

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
+// @flow
 
-const App = () => <h1>asd!</h1>;
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    input: '',
+  };
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
